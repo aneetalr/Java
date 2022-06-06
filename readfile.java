29e8d059dcbffacc7b96e6651a6f19a12429c9a3
Aneeta L R <97457996+aneetalr@users.noreply.github.com>
2022-06-06 15:01:31 +0530

    Add files via upload

---
+++ readfile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class readfile {
+   public static void main(String[] args) {
+      String s="";
+      int v;
+      BufferedReader br = null;
+       
+       try{	
+           br = new BufferedReader(new FileReader("F:/Aneeta/num.txt"));		
+		while((s=br.readLine())!=null){
+		   v=Integer.parseInt(s);
+			if (v%2==0)
+			System.out.println("Even= "+v);
+			else
+			System.out.println("ODD= "+v);
+}
+		 } 
+       catch (IOException ioe) 
+       {
+	   ioe.printStackTrace();
+       } 
+}}
---
+++ server.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+
+class server{
+    public static void main(String args[]){
+        try{
+            ServerSocket sock=new ServerSocket(4567);
+            Socket soc=sock.accept();
+	BufferedReader ser_is = new BufferedReader(new InputStreamReader(System.in));
+                
+	DataOutputStream outToclient =new DataOutputStream(soc.getOutputStream());
+		
+	DataInputStream dis=new DataInputStream(soc.getInputStream());
+            System.out.println("Connection Established");
+            String msg1 =dis.readUTF();
+            System.out.println("MESSAGE : "+msg1);
+		outToclient.writeUTF(ser_is.readLine());
+          
+            soc.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+
+        }
+    }
+}
